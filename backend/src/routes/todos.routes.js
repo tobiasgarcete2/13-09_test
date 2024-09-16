20597fd8b24f61225cd7eebea3de63f24d038e90
Your Name <you@example.com>
2024-09-16 16:39:59 -0300

    se agrego la funcionalidad en el backend

--- backend/src/routes/todos.routes.js
+++ backend/src/routes/todos.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getAllTodosCtrl } from "../controllers/todos.controllers.js";
+import { addTodoCtrl, deleteTodoCtrl, getAllTodosCtrl, updateTodoCtrl } from "../controllers/todos.controllers.js";
 import validarJwt from "../middlewares/validar-jwt.js";
 
 const todosRouter = Router();
 
-todosRouter.get("/", validarJwt, getAllTodosCtrl);
+todosRouter.get("/",validarJwt, getAllTodosCtrl);
+
+todosRouter.put("/:id",validarJwt, updateTodoCtrl);
+
+todosRouter.delete("/:id",validarJwt, deleteTodoCtrl)
+
+todosRouter.post("/agregar",validarJwt, addTodoCtrl)
 
 export { todosRouter };
